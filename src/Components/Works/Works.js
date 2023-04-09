6b3b6607d9f287790697602c2c5589a0867667cc
masum45299 <masum459299@gmail.com>
2023-04-10 00:31:26 +0600

    works

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import upworks from '../../img/Upwork.png';
+import fiver from '../../img/fiverr.png';
+import shopify from '../../img/Shopify.png';
+import facebook from '../../img/Facebook.png';
+import amazon from '../../img/amazon.png';
 import './Works.css'
 
 const Works = () => {
@@ -12,6 +17,28 @@ const Works = () => {
                 <button className='button s-button'>Download CV</button>
                 <div className='blur s-blur' style={{background:"#ABF1FF94"}}>
                 </div>
+            </div>
+            <div className='w-right'>
+                <div className='w-maincircle'>
+                    <div className='w-secondcircle'>
+                        <img src={upworks} alt='' />
+                    </div>
+                    <div className='w-secondcircle'>
+                        <img src={fiver} alt='' />
+                    </div>
+                    <div className='w-secondcircle'>
+                        <img src={amazon} alt='' />
+                    </div>
+
+                    <div className='w-secondcircle'>
+                        <img src={shopify} alt='' />
+                    </div>
+                    <div className='w-secondcircle'>
+                        <img src={facebook} alt='' />
+                    </div>
+                </div>
+                <div className='w-bgcircle bluecircle'></div>
+                <div className='w-bgcircle yellowcircle'></div>
             </div>
         </div>
     );
