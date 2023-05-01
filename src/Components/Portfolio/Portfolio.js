77ed527937a6fdce3f9f4ded85e14c19ee4c3571
masum45299 <masum459299@gmail.com>
2023-05-02 00:21:19 +0600

    swiper

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import sideBar from '../../img/sidebar.png';
+import ecommerce from '../../img/ecommerce.png';
+import Hoc from '../../img/hoc.png';
+import MusicApp from '../../img/musicapp.png';
+import 'swiper/css';
+import './Portfolio.css'
 
 const Portfolio = () => {
     return (
-        <div>
-            This is portfolio
-        </div>
+       <div className='portfolio'>
+            {/* Heading */}
+            <span>Recent Projects</span>
+            <span>Portfolio</span>
+
+            {/* Slider */}
+            <Swiper
+            spaceBetween={30}
+            slidesPerView={3}
+            >
+                <SwiperSlide>
+                    <img src={sideBar} alt='' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={ecommerce} alt='' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Hoc} alt='' />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={MusicApp} alt='' />
+                </SwiperSlide>
+            </Swiper>
+
+       </div>
     );
 };
 
